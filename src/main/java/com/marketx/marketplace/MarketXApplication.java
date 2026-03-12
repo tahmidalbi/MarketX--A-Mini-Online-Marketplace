db3489c1c276ee35b3720b46361384b04316ce15
tahmidalbi <albitahmid@gmail.com>
2026-03-12 17:25:48 +0600

    initial setup completed

---
+++ src/main/java/com/marketx/marketplace/MarketXApplication.java
@@ -0,0 +1,13 @@
+package com.marketx.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketx/marketplace/MarketXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketx.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
